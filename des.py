7a67cc781977e373612249e60b656be8821d6c80
Nick van Santen <vansantennick@gmail.com>
2021-11-28 14:56:12 +0100

    Obtain avg wait and process times

--- des.py
+++ des.py
@@ -1,12 +1,18 @@
+import numpy as np
 import random
 import simpy
 
 INIT_TASKS = 4
-N_MACHINES = 3
+N_MACHINES = 1
 MAX_SIM_TIME = 100
 TASK_DURATION = 10
 TASK_CREATION = 3
 TASK_CREATION_SPREAD = 2
+
+data = {
+    "wait_times": [],
+    "process_times": [],
+}
 
 class Server():
 
@@ -35,12 +41,19 @@ class Task():
 def process_task(env, task, server):
 
     print(f"{task} added to server at {env.now}. Queue length: {server.queue_length}")
+    time_at_queue = env.now
     with server.machine.request() as request:
         yield request
 
         print(f"{task} is being processed at {env.now}")
+        time_start_process = env.now
         yield env.process(server.run_task(task))
         print(f"{task} is completed at {env.now}")
+        time_end_process = env.now
+
+    data["wait_times"].append(time_start_process - time_at_queue)
+    data["process_times"].append(time_end_process - time_start_process)
+
 
 def setup(env, n_machines):
 
@@ -59,6 +72,9 @@ def main():
     env = simpy.Environment()
     env.process(setup(env, N_MACHINES))
     env.run(until=MAX_SIM_TIME)
+
+    print(f"Avg wait time: {np.mean(data['wait_times'])}")
+    print(f"Avg process time: {np.mean(data['process_times'])}")
 
 if __name__ == "__main__":
     main()
